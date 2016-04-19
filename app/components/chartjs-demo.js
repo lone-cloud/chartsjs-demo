5dbe60294feeef3d70f06513e8c5f243c7bb01b0
Egor Philippov <ephilippov@absolute.com>
2016-04-19 16:54:55 -0700

    skeleton

---
+++ app/components/chartjs-demo.js
@@ -0,0 +1,13 @@
+import Em from 'ember';
+
+export default Em.Component.extend({
+  elementId: 'chartjs-demo',
+  
+  bar1: null,
+  bar2: null,
+  line1: null,
+  
+  didInsertElement(){
+    
+  }
+});
---
+++ ember-cli-build.js
@@ -0,0 +1,14 @@
+/*jshint node:true*/
+/* global require, module */
+var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+module.exports = function(defaults) {
+  var app = new EmberApp(defaults, {
+    // Add options here
+  });
+
+  app.import('bower_components/bootstrap-sass/assets/javascripts/bootstrap/tab.js');
+  app.import('bower_components/Chart.js/dist/Chart.js');
+
+  return app.toTree();
+};
